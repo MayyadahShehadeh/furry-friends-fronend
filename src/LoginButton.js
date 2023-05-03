81b27979749a1d3a1f4f384eaadc2a335011bcc5
MayyadahShehadeh <miss.mayadah1@gmail.com>
2023-05-03 16:04:44 +0000

    add some css and login page

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,10 +1,19 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import Button from '@mui/material/Button';
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <Button
+  type="submit"
+  fullWidth
+  variant="contained"
+  sx={{ mt: 3, mb: 2 }}
+  onClick={() => loginWithRedirect()}
+>Log In
+  </Button>
+
 };
 
 export default LoginButton;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react'
+import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
+// import './css/bootstrap.min.css'
+// import './css/responsive.css'
+// import Carousel from 'react-bootstrap/Carousel';
+import image1 from './images/Website_HeroBanner_TNRCatsnip.jpg'
+import image2 from './images/3858438.jpg'
+import image3 from './images/5340927.jpg'
+
+
 
 export class Home extends Component {
+
+  
   render() {
     return (
-      <div>Home</div>
+      <>
+       <MDBCarousel showIndicators showControls fade>
+      <MDBCarouselItem
+        className="w-100 h-50 d-block"
+        itemId={1}
+        src={image1}
+        alt="..."
+      >
+        {/* <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+      </MDBCarouselItem>
+
+      <MDBCarouselItem
+        className="w-100 d-block"
+        itemId={2}
+        src={image2}
+        alt="..."
+      >
+        {/* <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+      </MDBCarouselItem>
+
+      <MDBCarouselItem
+        className="w-100 d-block"
+        itemId={3}
+        src={image3}
+        alt="..."
+      >
+        {/* <h5>Third slide label</h5>
+        <p>
+          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+        </p> */}
+      </MDBCarouselItem>
+    </MDBCarousel>
+
+    <br/> <br/><br/><br/><br/>
+      </>
     )
   }
 }
